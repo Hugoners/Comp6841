32e15b250652e425f2b90d6977ca717c06541732
Hugo Ng <z5358562@ad.unsw.edu.au>
2023-11-03 12:02:54 +1100

    not looking good

--- irc.py
+++ irc.py
@@ -1,12 +1,13 @@
 import threading
 import socket
 import logging
+from Crypto.PublicKey import RSA
 from dataclasses import dataclass
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-bind_addr = ('127.0.0.1', 6441)
+bind_addr = ('127.0.0.1', 6841)
 server = socket.socket()
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(bind_addr)
@@ -17,6 +18,7 @@ server.listen()
 class User:
     ret_addr: int
     client: socket
+    publicKey = rsa.key.PublicKey = None
     nick: str = None
     user: str = None
     channels: list = None
@@ -32,8 +34,7 @@ users = {}
 
 def broadcast_all(message):
     for client in clients:
-        log.info(f"here {client}")
-        client.send(f"SERVER: {message}".encode())
+        client.send(f"SERVER: {message}".encode('utf8'))
 
 
 def nick_check(message, addr):
@@ -66,7 +67,7 @@ def privmsg(message):
 
     for user in users.keys():
         if users[user].user == receiver:
-            users[user].client.send(f"hi please work".encode())
+            users[user].client.send(f"hi please work".encode('utf8'))
             log.info(f"{command}, {receiver}, {message_to_send}")
 
     # return f"{message_to_send}"
@@ -76,7 +77,10 @@ def message_handle(client, addr):
     connected = True
     while connected:
         try:
-            message = client.recv(1024).decode()
+            message = client.recv(1024)
+            log.info(f"{message}")
+            rsa.key.PublicKey.load_pkcs1(message.encode('utf8'))
+            # log.warning(f"{b}")
             log.info(f"{client}, {message}")
             # command = message.split()[0]
             user = users[addr]
@@ -86,10 +90,10 @@ def message_handle(client, addr):
 
             if user.nick is None:
                 msg = nick_check(message, addr)
-                client.send(msg.encode())
+                client.send(msg.encode('utf8'))
             elif user.user is None:
                 msg = reg_user(message, addr)
-                client.send(msg.encode())
+                client.send(msg.encode('utf8'))
             else:
                 privmsg(message)
                 broadcast_all(message)
@@ -106,7 +110,8 @@ def message_handle(client, addr):
             nicks.remove(disconnected_nick)
             client.close()
             connected = False
-            broadcast_all(f"{disconnected_nick} has disconncted!".encode())
+            broadcast_all(
+                f"{disconnected_nick} has disconncted!".encode('utf8'))
             log.info(f"{users} remaining")
 
 
@@ -122,9 +127,19 @@ def connection_handler():
         # nickname = client.recv(1024).decode()
         # nicks.append(nickname)
         clients.append(client)
+        # publicKey, privateKey = rsa.newkeys(512)
+        # client.send(privateKey)
         user = User(addr[1], client)
         users[addr[1]] = user
 
+        # # log.info(f"{type(publicKey)}, {type(privateKey)}")
+        # log.info(f"{publicKey}")
+        # log.info(f"{privateKey}")
+
+        # test_message = 'h'.encode('utf8')
+        # test = rsa.encrypt(test_message, publicKey)
+        # log.info(f"{test}")
+        # log.info(f"{type(test)}")
         thread = threading.Thread(
             target=message_handle, args=(client, addr[1]))
         thread.start()
--- irc_client.py
+++ irc_client.py
@@ -1,18 +1,21 @@
 import threading
 import socket
+import rsa
 
 print("Please register a nick using the command NICK nickname (Must not include space)")
 # nickname = input()
 
-bind_addr = ('127.0.0.1', 6441)
+bind_addr = ('127.0.0.1', 6841)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(bind_addr)
 
 
 def receive():
+    # private_key = client.recv(1024)
+    # print(private_key)
     while True:
         try:
-            message = client.recv(1024).decode()
+            message = client.recv(1024).decode('utf8')
             # if message == 'NICK':
             #     client.send(nickname.encode())
             # else:
@@ -24,9 +27,14 @@ def receive():
 
 
 def write():
+    pub_key, privateKey = rsa.newkeys(2048)
+
+    pub_key = pub_key.save_pkcs1()
+    client.send(pub_key)
+
     while True:
         message = input()
-        client.send(message.encode())
+        client.send(message.encode('utf8'))
 
 
 receive_thread = threading.Thread(target=receive)
